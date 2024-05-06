8bb8fe09828ca71bf7dc95a70b47707f220ea889
satyajay01 <satyajaydibya0@gmail.com>
2024-05-06 18:45:58 +0530

    Image uploading and Image handling

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,8 +3,23 @@ import './login.css'
 
 const Login = () => {
 
+    //Image uploading and handling------
+    const [avatar,setAvatar] = useState({
+        file:null,
+        url:""
+    })
 
-    return (
+    const handleAvatar = e =>{
+        if(e.target.files[0]){
+            setAvatar({
+                file:e.target.files[0],
+                url: URL.createObjectURL(e.target.files[0])
+            })
+        }
+    }
+
+
+  return (
     <div className='login'>
         <div className='item'>
             <h2>Welcome Back</h2>
@@ -18,9 +33,9 @@ const Login = () => {
         <div className='item'>
         <h2>Create an Account</h2>
             <form>
-                <label htmlFor="file"><img src="" alt="" />Upload an Image</label>
-                <input type="file" id='file' style={{display: "none"}}/>
-
+                <label htmlFor="file"><img src={avatar.url || "./avatar.png"} alt="" />Upload an Image</label>
+                <input type="file" id='file' style={{display: "none"}} onChange={handleAvatar}/>
+                
                 <input type="text" placeholder='Username' name='username'/>
                 <input type="email" placeholder='Email' name='email'/>
                 <input type="password" placeholder='Password' name='password'/>
